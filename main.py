1d43b750a3617bcdc3ddd68093dde83197429c14
Tyler Gabriel <tygabriel7@gmail.com>
2019-02-09 16:21:29 -0500

    Parse Tweet Data
    Reads tweets but does not store data yet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from twython import Twython
 import time
+import json
 
 
 
@@ -11,43 +12,39 @@ apiSecret = "mQ0w0YyeY5z5WkkDByI3TG9kI9OWzmmKGoe9A9BLEPeAhbm6Sj"
 accessToken = "832293691888918533-6Mjald6xTQoGQw5UsLjyUNqkYbo1Ipo"
 accessTokenSecret = "PKJJzwCU05e7UHgRtoRdVTYDRjZ48ksewf4arpQwlw18S"
 
+#tweetStr="Hey, Jesse White!"
+api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
+#api.update_status(status=tweetStr)
 
 
 
 class song:
-    def __init__(name, tempo, notes, key, user):
+    def __init__(self, user, name, recp, notes):
+        self.user= user
         self.name = name
-        self.age = age
-        self.tempo = tempo
+        self.recp = recp
         self.notes = notes
-        self.key = key
-        self.user = user
         self.played = False
     def play(self):
         print("Playing: "+ self.name)
 
-bool=l
-while
+tweet = api.get_mentions_timeline()
 
+results = api.search(q="@project_mayo",count = 10);
+all_tweets = results['statuses']
+for tw in all_tweets:
+    print(tw['text'])
 
+#data = json.loads(tweet)
+#print(tweet)
 
+#location = content.find("u'text'")
+#end = content.find("DONE")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-tweetStr="Hey, Jesse White!"
-api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
-api.update_status(status=tweetStr)
+#loop = True
+#while loop:
+#    print("loopdeloopandpull")
+#    loop = False
+#tweetStr="Hey, Jesse White!"
+#api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
+#api.update_status(status=tweetStr)

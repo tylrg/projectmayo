517544563e8ebd6df521449e19ad66eae7806560
Tyler Gabriel <tygabriel7@gmail.com>
2019-02-12 21:30:51 -0500

    Waiting for Midi
    Waiting for Midi export to finish and make more robust

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import os
 import re
 import dropbox
+import subprocess
 
 # Secret Keys
 apiKey = "qaiHFqWKyHuHWibbBOuJl3grj"
@@ -12,9 +13,7 @@ apiSecret = "mQ0w0YyeY5z5WkkDByI3TG9kI9OWzmmKGoe9A9BLEPeAhbm6Sj"
 accessToken = "832293691888918533-6Mjald6xTQoGQw5UsLjyUNqkYbo1Ipo"
 accessTokenSecret = "PKJJzwCU05e7UHgRtoRdVTYDRjZ48ksewf4arpQwlw18S"
 
-#tweetStr="Hey, Jesse White!"
 api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
-#api.update_status(status=tweetStr)
 
 tweet = api.get_mentions_timeline()
 results = api.search(q="@project_mayo",count = 10);
@@ -36,15 +35,12 @@ for x in song_List:
 #everything is in content
 songNameLoc = content.find("NAME:")+6
 toLoc = content.find("TO")
-
 songName = content[songNameLoc:toLoc-1]
-print(songName)
-
+print("Song Name: "+songName)
 toLoc += 4
 #print(content[toLoc])
 recp = content[toLoc:content.find("!")]
-print(recp)
-
+print("To:" + recp)
 end = content.find("DONE")
 notes = content[content.find("!")+2:end]
 print(notes)
@@ -53,36 +49,31 @@ print(notes)
 
 #Name of file
 songName = songName.replace(" ","_")
-# print(songName)
 
-#conversion section
+#conversion section for alda language
 notes = notes.replace ("&lt;","<")
 notes = notes.replace ("&gt;",">")
 
-print(songName)
+
+#printing songName and writing to alda
+print("Playing:" + songName)
 test = songName + ".txt"
 test_object = open(test,"w")
 test_object.write(notes)
 test_object.close()
 os.system("./alda play -f "+test)
 
+#record system audio to a file
+
+
+if len(songName)>0 :
+    #upload to Dropbox
+    dbx = dropbox.Dropbox("NViWwhEdVtAAAAAAAAAAWGmCiE9SH9BE097WfoRq4l8Hif21_OLbEb_F0TvYI4KC")
+    file = open("/Users/tylergabriel/Desktop/CS/projectmayo/"+songName+".txt")
+    dbx.files_upload(file.read(),"/"+songName+".txt")
 
 
 
-
-
-
-
-
-
-
-
-
-
-dbx = dropbox.Dropbox("NViWwhEdVtAAAAAAAAAAWGmCiE9SH9BE097WfoRq4l8Hif21_OLbEb_F0TvYI4KC")
-file = open("/Users/tylergabriel/Desktop/CS/projectmayo/"+songName+".txt")
-dbx.files_upload(file.read(),"/"+songName+".txt")
-
-
-tweetStr="Hey,"+recp+"! Here is a link to "+ songName+" , a song made just for you! https://www.dropbox.com/home/Apps/projectmayo?preview="+songName+".txt"
-api.update_status(status=tweetStr)
+    #reply to user
+    tweetStr="Hey,"+recp+"! Here is a link to "+ songName+" , a song made just for you! https://www.dropbox.com/home/Apps/projectmayo?preview="+songName+".txt"
+    api.update_status(status=tweetStr)

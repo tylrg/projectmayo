45615699dda48d0de2b26105eb1cc8e7d591c984
Tyler Gabriel <31976417+tylrg@users.noreply.github.com>
2019-06-28 23:47:17 -0400

    Redacted

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import dropbox
 import subprocess
 
 # Secret Keys
-apiKey = "qaiHFqWKyHuHWibbBOuJl3grj"
-apiSecret = "mQ0w0YyeY5z5WkkDByI3TG9kI9OWzmmKGoe9A9BLEPeAhbm6Sj"
-accessToken = "832293691888918533-6Mjald6xTQoGQw5UsLjyUNqkYbo1Ipo"
-accessTokenSecret = "PKJJzwCU05e7UHgRtoRdVTYDRjZ48ksewf4arpQwlw18S"
+apiKey = ""
+apiSecret = ""
+accessToken = ""
+accessTokenSecret = ""
 
 api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
 
